324b1a8846f8ff2a63dd4f759d9b114d70588de8
Feroz Ghauri <mferozghauri@gmail.com>
2021-04-29 00:29:58 +0500

    Section 5 pre Assignment 2

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import './Expenses.css'
-import ExpenseItem from './ExpenseItem';
 import ExpensesFilter from './ExpensesFilter';
 import Card from '../UI/Card';
+import ExpensesList from './ExpensesList';
 
 const Expenses = (props) => {
 
@@ -17,17 +17,7 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === yearSelected
   });
 
-  let expensesContent = <p>No Expenses Found. You saved a lot of Money you are now  a billionairre</p>
 
-  if (filteredYearArray.length >0 ){
-    expensesContent = filteredYearArray.map((expense) => (
-      <ExpenseItem 
-        key={expense.id}
-        title={expense.title} 
-        amount={expense.amount}
-        date={expense.date}
-        />))
-  }
 
 
    return(
@@ -35,7 +25,7 @@ const Expenses = (props) => {
 
     <Card className="expenses">
       <ExpensesFilter selected={yearSelected} onYearChange={yearChangeHandler}/>
-      {expensesContent}
+      <ExpensesList items = {filteredYearArray}/>
     </Card>
        
    );
